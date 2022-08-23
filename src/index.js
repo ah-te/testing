85d417bf42c09dc965ddbb611a4553b4208a03ba
Fatihu Olalekan Sanusi <70672530+ah-te@users.noreply.github.com>
2022-08-23 23:48:49 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(
+  <div>
+    <h1>
+      My favoyrite food
+      </h1>
+      <ul>
+        <li>rice</li>
+        <li>swallow</li>
+        <li>fruit</li>
+      </ul>
+  </div>,document.getElementById("root")
+)
